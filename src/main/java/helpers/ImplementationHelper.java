aa3b57cc505e73072f3939c4f89579e2fbd48701
jobaer <jobaer.du@gmail.com>
2016-11-12 14:06:20 +0600

    Add missing domain and helper class

---
+++ src/main/java/helpers/ImplementationHelper.java
@@ -0,0 +1,26 @@
+package helpers;
+
+import org.apache.commons.lang3.NotImplementedException;
+import rx.Observable;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+public class ImplementationHelper {
+    public static <T> T todo() {
+        throw new NotImplementedException("Todo");
+    }
+
+    public static void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static <T> Observable<T> withRandomDelay(Observable<T> sourceObservable) {
+        int randomDelay = ThreadLocalRandom.current().nextInt(500, 1000);
+        return sourceObservable.delay(randomDelay, TimeUnit.MILLISECONDS);
+    }
+}
