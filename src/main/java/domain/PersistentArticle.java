a842111693ea233f7ab069c63a7a45c4765c6869
jobaer <jobaer.du@gmail.com>
2017-01-22 10:29:05 +0600

    Add helper classes

---
+++ src/main/java/domain/PersistentArticle.java
@@ -0,0 +1,19 @@
+package domain;
+
+public class PersistentArticle {
+    private Integer id;
+    private String title;
+
+    public PersistentArticle(Integer id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
