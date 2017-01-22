3f3fe8a2d4e8aad87995d0796c5ac67fa51415b0
jobaer <jobaer.du@gmail.com>
2017-01-22 10:30:06 +0600

    Second set of exercises for day1. Basic operators.

---
+++ src/main/java/basics/BasicOperations.java
@@ -0,0 +1,39 @@
+package basics;
+
+import domain.Person;
+import rx.Observable;
+
+import java.util.List;
+
+/**
+ * Few basic operations
+ */
+public class BasicOperations {
+    public static void main(String[] args) {
+        System.out.println("Testing basic operations");
+    }
+
+    Observable<Integer> filterEven(Observable<Integer> numbers) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    Observable<Person> filterBelowFifty(Observable<Person> persons) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    Observable<Person> getPerson(Integer id) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    Observable<Person> getPersons(List<Integer> idList) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    Observable<Person> getPersonSafe(Integer id) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    Observable<Person> getPersonsSafe(List<Integer> idList) {
+        throw new RuntimeException("Not implemented");
+    }
+}
