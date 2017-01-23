3d655fe15d5baaf63fcc37658e80bf19d11653fc
jobaer <jobaer.du@gmail.com>
2017-01-23 22:23:38 +0600

    subscribeOn and observeOn

--- src/main/java/concurrency/BasicConcurrency.java
+++ src/main/java/concurrency/BasicConcurrency.java
@@ -1,12 +1,14 @@
 package concurrency;
 
 import rx.Observable;
+import rx.schedulers.Schedulers;
 
+import static helpers.ImplementationHelper.sleep;
 import static helpers.ImplementationHelper.threadName;
 
 public class BasicConcurrency {
     public static void main(String[] args) {
-        basicSubscribe();
+        testObserveOn();
     }
 
     private static void basicSubscribe() {
@@ -16,5 +18,28 @@ public class BasicConcurrency {
         lengths.subscribe(len -> {
             System.out.println("Received " + len + " on thread " + threadName());
         });
+    }
+
+    private static void testSubscribeOn() {
+        Observable<String> values = Observable.just("Alpha", "Beta", "Gamma");
+
+        values.subscribeOn(Schedulers.io())
+                .map(String::length)
+                .subscribe(len -> {
+                    System.out.println("Received " + len + " on thread " + threadName());
+                });
+        sleep(3000);
+    }
+
+    private static void testObserveOn() {
+        Observable<Integer> source = Observable.range(1, 10);
+
+        source.map(i -> i * 100)
+                .doOnNext(i -> System.out.println("Emitting " + i + " on thread " + threadName()))
+                .observeOn(Schedulers.computation())
+                .map(i -> i * 10)
+                .subscribe(i -> System.out.println("Received " + i + " on thread " + threadName()));
+
+        sleep(3000);
     }
 }
